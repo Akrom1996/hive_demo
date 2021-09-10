19e3fed70dcd19d965589fa84677ada565f3ceb2
Akrom <akobidov777@gmail.com>
2021-09-10 14:25:11 +0500

    sadsad

--- lib/service/hive.dart
+++ lib/service/hive.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 import 'package:hive/hive.dart';
 import 'package:hive_example/model/User.dart';
 
+
 class HiveDB{
   static final box = Hive.box("user");
    saveData(String user /*User user*/){
